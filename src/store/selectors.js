fd05dd97961f47ddb0199bf99fb494c985dfe10d
hylaron <53276116+hylaron@users.noreply.github.com>
2023-06-30 15:28:16 +0300

    Delete src directory

--- src/store/selectors.js
+++
@@ -1,9 +0,0 @@
-export const getShedule = (state) => state.shedule;
-
-export const getGrade = (state) => state.grade;
-
-export const getOrders = (state) => state.orders;
-
-export const getPayPage = (state) => state.payPage;
-
-export const getCard = (state) => state.card;
